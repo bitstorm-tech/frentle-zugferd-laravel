0a173906dac027e53946efd15b5d4b220820129e
Josef Bauer <josef.bauer.1st@gmail.com>
2025-03-23 15:56:17 +0100

    Replace switch with match

--- app/Services/XmlService.php
+++ app/Services/XmlService.php
@@ -38,152 +38,121 @@ class XmlService
 
     private function setXmlData(ZugferdDocumentBuilder $document, string $key, string|array $value): void
     {
-        Log::info("Add to invoice XML: {$key} => ".print_r($value, true));
+        // Log::info("Add to invoice XML: {$key} => ".print_r($value, true));
 
-        switch ($key) {
-            case 'notes':
-                $document->addDocumentNote($value);
-                break;
-            case 'billingPeriod':
-                $document->setDocumentBillingPeriod(
-                    DateTime::createFromFormat('Ymd', '20250101'),
-                    DateTime::createFromFormat('Ymd', '20253101'),
-                    $value
-                );
-                break;
-            case 'tenderOrLotReferenceDocument':
-                $document->addDocumentTenderOrLotReferenceDocument($value);
-                break;
-            case 'invoicedObjectReferenceDocument':
-                $document->addDocumentInvoicedObjectReferenceDocument(
-                    $value,
-                    ZugferdReferenceCodeQualifiers::SALE_PERS_NUMB
-                );
-                break;
-            case 'contractReferencedDocument':
-                $document->setDocumentContractReferencedDocument($value);
-                break;
-            case 'procuringProject':
-                $document->setDocumentProcuringProject($value);
-                break;
-            case 'paymentMeanToDirectDebit':
-                $document->addDocumentPaymentMeanToDirectDebit($value);
-                break;
-            case 'paymentTerm':
-                $document->addDocumentPaymentTerm($value);
-                break;
-            case 'sellerName':
-                $document->setDocumentSeller($value);
-                break;
-            case 'sellerGlobalId':
-                $document->addDocumentSellerGlobalId($value);
-                break;
-            case 'sellerTaxNumber':
-                $document->addDocumentSellerTaxNumber($value);
-                break;
-            case 'sellerVATRegistrationNumber':
-                $document->addDocumentSellerVATRegistrationNumber($value);
-                break;
-            case 'sellerAddress':
-                $document->setDocumentSellerAddress(
-                    $value['line1'],
-                    $value['line2'],
-                    $value['line3'],
-                    $value['postCode'],
-                    $value['city'],
-                    $value['country'],
-                    $value['subDivision'],
-                );
-                break;
-            case 'sellerContact':
-                $document->setDocumentSellerContact(
-                    $value['contactPersonName'],
-                    $value['contactDepartmentName'],
-                    $value['contactPhoneNo'],
-                    $value['contactFaxNo'],
-                    $value['contactEmailAddress']
-                );
-                break;
-            case 'sellerCommunication':
-                $document->setDocumentSellerCommunication(ZugferdElectronicAddressScheme::UNECE3155_EM, $value);
-                break;
-            case 'buyerName':
-                $document->setDocumentBuyer($value);
-                break;
-            case 'buyerAddress':
-                $document->setDocumentBuyerAddress(
-                    $value['line1'],
-                    $value['line2'],
-                    $value['line3'],
-                    $value['postCode'],
-                    $value['city'],
-                    $value['country'],
-                    $value['subDivision'],
-                );
-                break;
-            case 'buyerContact':
-                $document->setDocumentBuyerContact(
-                    $value['contactPersonName'],
-                    $value['contactDepartmentName'],
-                    $value['contactPhoneNo'],
-                    $value['contactFaxNo'],
-                    $value['contactEmailAddress']
-                );
-                break;
-            case 'buyerCommunication':
-                $document->setDocumentBuyerCommunication(ZugferdElectronicAddressScheme::UNECE3155_EM, $value);
-                break;
-            case 'payee':
-                $document->setDocumentPayee($value);
-                break;
-            case 'buyerOrderReferencedDocument':
-                $document->setDocumentBuyerOrderReferencedDocument($value);
-                break;
-            case 'sellerOrderReferencedDocument':
-                $document->setDocumentSellerOrderReferencedDocument($value);
-                break;
-            case 'shipTo':
-                $document->setDocumentShipTo($value);
-                break;
-            case 'shipToAddress':
-                $document->setDocumentShipToAddress(
-                    $value['line1'],
-                    $value['line2'],
-                    $value['line3'],
-                    $value['postCode'],
-                    $value['city'],
-                    $value['country'],
-                    $value['subDivision'],
-                );
-                break;
-            case 'deliveryDate':
-                $document->setDocumentSupplyChainEvent(DateTime::createFromFormat('Ymd', '20250101'));
-                break;
-            case 'positions':
-                $this->addPositions($document, $value);
-                break;
-            case 'tax':
-                $document->addDocumentTax(
-                    ZugferdVatCategoryCodes::STAN_RATE,
-                    ZugferdVatTypeCodes::VALUE_ADDED_TAX,
-                    (float) $value['basisAmount'],
-                    (float) $value['calculatedAmount'],
-                    (float) $value['rateApplicablePercent'],
-                );
-                break;
-            case 'summation':
-                $document->setDocumentSummation(
-                    (float) $value['grandTotalAmount'],
-                    (float) $value['duePayableAmount'],
-                );
-                break;
-            case 'invoiceNumber':
-                // do nothing but also don't print an error
-                break;
+        match ($key) {
+            'notes' => $document->addDocumentNote($value),
 
-            default:
-                Log::error("Unknown key: '{$key}'");
-        }
+            'billingPeriod' => $document->setDocumentBillingPeriod(
+                DateTime::createFromFormat('Ymd', '20250101'),
+                DateTime::createFromFormat('Ymd', '20253101'),
+                $value
+            ),
+
+            'tenderOrLotReferenceDocument' => $document->addDocumentTenderOrLotReferenceDocument($value),
+
+            'invoicedObjectReferenceDocument' => $document->addDocumentInvoicedObjectReferenceDocument(
+                $value,
+                ZugferdReferenceCodeQualifiers::SALE_PERS_NUMB
+            ),
+
+            'contractReferencedDocument' => $document->setDocumentContractReferencedDocument($value),
+
+            'procuringProject' => $document->setDocumentProcuringProject($value),
+
+            'paymentMeanToDirectDebit' => $document->addDocumentPaymentMeanToDirectDebit($value),
+
+            'paymentTerm' => $document->addDocumentPaymentTerm($value),
+
+            'sellerName' => $document->setDocumentSeller($value),
+
+            'sellerGlobalId' => $document->addDocumentSellerGlobalId($value),
+
+            'sellerTaxNumber' => $document->addDocumentSellerTaxNumber($value),
+
+            'sellerVATRegistrationNumber' => $document->addDocumentSellerVATRegistrationNumber($value),
+
+            'sellerAddress' => $document->setDocumentSellerAddress(
+                $value['line1'],
+                $value['line2'],
+                $value['line3'],
+                $value['postCode'],
+                $value['city'],
+                $value['country'],
+                $value['subDivision'],
+            ),
+
+            'sellerContact' => $document->setDocumentSellerContact(
+                $value['contactPersonName'],
+                $value['contactDepartmentName'],
+                $value['contactPhoneNo'],
+                $value['contactFaxNo'],
+                $value['contactEmailAddress']
+            ),
+
+            'sellerCommunication' => $document->setDocumentSellerCommunication(ZugferdElectronicAddressScheme::UNECE3155_EM, $value),
+
+            'buyerName' => $document->setDocumentBuyer($value),
+
+            'buyerAddress' => $document->setDocumentBuyerAddress(
+                $value['line1'],
+                $value['line2'],
+                $value['line3'],
+                $value['postCode'],
+                $value['city'],
+                $value['country'],
+                $value['subDivision'],
+            ),
+
+            'buyerContact' => $document->setDocumentBuyerContact(
+                $value['contactPersonName'],
+                $value['contactDepartmentName'],
+                $value['contactPhoneNo'],
+                $value['contactFaxNo'],
+                $value['contactEmailAddress']
+            ),
+
+            'buyerCommunication' => $document->setDocumentBuyerCommunication(ZugferdElectronicAddressScheme::UNECE3155_EM, $value),
+
+            'payee' => $document->setDocumentPayee($value),
+
+            'buyerOrderReferencedDocument' => $document->setDocumentBuyerOrderReferencedDocument($value),
+
+            'sellerOrderReferencedDocument' => $document->setDocumentSellerOrderReferencedDocument($value),
+
+            'shipTo' => $document->setDocumentShipTo($value),
+
+            'shipToAddress' => $document->setDocumentShipToAddress(
+                $value['line1'],
+                $value['line2'],
+                $value['line3'],
+                $value['postCode'],
+                $value['city'],
+                $value['country'],
+                $value['subDivision'],
+            ),
+
+            'deliveryDate' => $document->setDocumentSupplyChainEvent(DateTime::createFromFormat('Ymd', '20250101')),
+
+            'positions' => $this->addPositions($document, $value),
+
+            'tax' => $document->addDocumentTax(
+                ZugferdVatCategoryCodes::STAN_RATE,
+                ZugferdVatTypeCodes::VALUE_ADDED_TAX,
+                (float) $value['basisAmount'],
+                (float) $value['calculatedAmount'],
+                (float) $value['rateApplicablePercent'],
+            ),
+
+            'summation' => $document->setDocumentSummation(
+                (float) $value['grandTotalAmount'],
+                (float) $value['duePayableAmount'],
+            ),
+
+            'invoiceNumber', 'buyerVATRegistrationNumber' => true, // do nothing but also don't print an error
+
+            default => Log::error("Unknown key: '{$key}'"),
+        };
     }
 
     private function addPositions(ZugferdDocumentBuilder $document, array $positions): void
